9571fbdc23229432a059f1095e1de7d232739650
Marco491973 <marco.dinic@gmail.com>
2023-05-14 08:06:19 +0200

    Changed title

--- web.py
+++ web.py
@@ -22,5 +22,5 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="Enter ToDo:", placeholder="Add a new todo...",
+st.text_input(label="ToDo-Eingabe:", placeholder="Add a new todo...",
               on_change=add_todo, key='new_todo')
